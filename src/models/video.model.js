68ed15a185baf0985a5aee54ee405f65a4d89698
tanmay-0017 <tanmay.ug21@nsut.ac.in>
2024-05-28 13:35:19 +0530

    updated video model

--- src/models/video.model.js
+++ src/models/video.model.js
@@ -4,41 +4,46 @@ import mongooseAggregatePaginate from "mongoose-aggregate-paginate-v2";
 const videoSchema = new Schema(
     {
         videoFile: {
-            type: String, //cloudinary url
-            required: true
+            type: {
+                url: String,
+                public_id: String,
+            },
+            required: true,
         },
         thumbnail: {
-            type: String, //cloudinary url
-            required: true
-        },
-        title: {
-            type: String, 
-            required: true
-        },
-        description: {
-            type: String, 
-            required: true
-        },
-        duration: {
-            type: Number, 
-            required: true
-        },
-        views: {
-            type: Number,
-            default: 0
-        },
-        isPublished: {
-            type: Boolean,
-            default: true
+            type: {
+                url: String,
+                public_id: String,
+            },
+            required: true,
         },
         owner: {
             type: Schema.Types.ObjectId,
-            ref: "User"
-        }
-
-    }, 
+            ref: "User",
+        },
+        title: {
+            type: String,
+            required: true,
+        },
+        description: {
+            type: String,
+            required: true,
+        },
+        duration: {
+            type: Number,
+            required: true,
+        },
+        views: {
+            type: Number,
+            defaultValue: 0,
+        },
+        isPublished: {
+            type: Boolean,
+            defaultValue: false,
+        },
+    },
     {
-        timestamps: true
+        timestamps: true,
     }
 )
 
