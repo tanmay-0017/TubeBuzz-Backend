f36439db72a080d1efc1193000caad58b2dffaee
tanmay-0017 <tanmay.ug21@nsut.ac.in>
2024-05-29 11:36:59 +0530

    updated subscription route

--- src/routes/subscription.routes.js
+++ src/routes/subscription.routes.js
@@ -9,8 +9,7 @@ import {verifyJWT} from "../middlewares/auth.middleware.js"
 const router = Router();
 router.use(verifyJWT); // Apply verifyJWT middleware to all routes in this file
 
-router
-    .route("/c/:channelId")
+router.route("/c/:channelId")
     .get(getSubscribedChannels)
     .post(toggleSubscription);
 
